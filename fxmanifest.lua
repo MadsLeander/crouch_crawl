d97dda5580fbe67f15dbf8c2c537768e37b4d3b8
Mads <madsleander@outlook.com>
2023-02-18 20:30:38 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+authors 'Mads'
+description 'Crouch & Crawl'
+version '1.0.0'
+
+client_scripts {
+    'config.lua',
+    'walkstyles.lua',
+    'client.lua'
+}
